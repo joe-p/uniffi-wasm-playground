5c96fa125c0dd0c3887b905b17f19ea2b85f8c18
Joe Polny <joepolny@gmail.com>
2024-12-01 10:35:17 -0500

    play with errors

--- consumers/python/app.py
+++ consumers/python/app.py
@@ -1,3 +1,18 @@
-from arithmetic import add
+from arithmetic import add, div, ArithmeticError, InternalError
 
-print(add(1, 2))
+MAX_U64 = 18446744073709551615
+
+try:
+    div(1, 0)
+except InternalError as e:
+    print(f"We caught a panic! {e.__class__.__name__}: {e}")
+
+try:
+    add(MAX_U64, 1)
+except ArithmeticError.IntegerOverflow as e:
+    print(f"We caught an error! {e.__class__.__name__}: {e}")
+
+try:
+    add(MAX_U64 + 1, 0)
+except ValueError as e:
+    print(f"We caught an error thrown by the binding! {e.__class__.__name__}: {e}")
