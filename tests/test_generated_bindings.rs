c9afa8fe025cb6e74e20a899593f88c61675e441
Joe Polny <joepolny@gmail.com>
2025-03-26 13:20:36 -0400

    rm uniffi example tests

--- tests/test_generated_bindings.rs
+++
@@ -1,6 +0,0 @@
-uniffi::build_foreign_language_testcases!(
-    "tests/bindings/test_playground.rb",
-    "tests/bindings/test_playground.py",
-    // "tests/bindings/test_playground.kts",
-    // "tests/bindings/test_playground.swift",
-);
